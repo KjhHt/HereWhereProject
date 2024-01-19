07a2bd97a145167c56d94db97f64442a9eff149c
KangYoonku <tom6198@naver.com>
2024-01-19 20:42:08 +0900

    파노라마,로그인창,사이드바

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,43 +1,80 @@
-<script setup>
-import { ref } from 'vue';
-import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon";
-const menuTitle=ref('HERE WHERE');
-const menuItems=ref([
-    {link:'#',name:'나의 여행 지도',tooltip:'나의 여행 지도',icon:'bx-map-alt'},
-    {link:'#',name:'나의 예약',tooltip:'나의 예약',icon:'bx-calendar-event'},
-    {link:'#',name:'나의 정보',tooltip:'나의 정보',icon:'bx-user-circle'},
-    {link:'#',name:'팔로우',tooltip:'팔로우',icon:'bx-user-plus'}
-]);
-const searchPlaceholder=ref('검색')
-const searchTooltip=ref('검색')
-const logoTitleColor=ref('#000')
-const bgColor=ref('#E7E7E7')
-const iconsColor=ref('#000')
-const secondaryColor=ref('#E7E7E7')
-const itemsTooltipColor=ref('#000')
-console.log(menuTitle);
-const menuItemsTextColor=ref('#000')
-const searchInputTextColor=ref('#000')
-const menuItemsHoverColor=ref('#000')
-const isMenuOpen=ref(false)
-</script>
 <template lang="">
-    <div>
-        <VueSidebarMenuAkahon
-        :menuTitle='menuTitle'
-        :menuItems='menuItems'
-        :bgColor='bgColor'
-        :iconsColor='iconsColor'
-        :secondaryColor='secondaryColor'
-        :itemsTooltipColor='itemsTooltipColor'
-        :menuItemsTextColor='menuItemsTextColor'
-        :searchInputTextColor='searchInputTextColor'
-        :menuItemsHoverColor='menuItemsHoverColor'
-        :logoTitleColor='logoTitleColor'
-        :searchPlaceholder='searchPlaceholder'
-        :searchTooltip='searchTooltip'
-        :isMenuOpen='isMenuOpen'/>
+    <div class="page-container">
+        <div class="sidebar" >
+            <VueSidebarMenuAkahon
+            :menuTitle='menuTitle'
+            :menuItems='menuItems'
+            :bgColor='bgColor'
+            :iconsColor='iconsColor'
+            :secondaryColor='secondaryColor'
+            :itemsTooltipColor='itemsTooltipColor'
+            :menuItemsTextColor='menuItemsTextColor'
+            :searchInputTextColor='searchInputTextColor'
+            :menuItemsHoverColor='menuItemsHoverColor'
+            :logoTitleColor='logoTitleColor'
+            :searchPlaceholder='searchPlaceholder'
+            :searchTooltip='searchTooltip'
+            :isMenuOpen='isMenuOpen'/>
+        </div>
     </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon";
+
+export default {
+  components: {
+    VueSidebarMenuAkahon
+  },
+  setup() {
+    const menuTitle = ref('');
+    const menuItems = ref([
+      {link:'#',name:'나의 여행 지도',tooltip:'나의 여행 지도',icon:'bx-map-alt'},
+      {link:'#',name:'나의 예약',tooltip:'나의 예약',icon:'bx-calendar-event'},
+      {link:'#',name:'나의 정보',tooltip:'나의 정보',icon:'bx-user-circle'},
+      {link:'#',name:'팔로우',tooltip:'팔로우',icon:'bx-user-plus'}
+    ]);
+    const searchPlaceholder = ref('검색');
+    const searchTooltip = ref('검색');
+    const logoTitleColor = ref('#000');
+    const bgColor = ref('#E7E7E7');
+    const iconsColor = ref('#000');
+    const secondaryColor = ref('#E7E7E7');
+    const itemsTooltipColor = ref('#000');
+    const menuItemsTextColor = ref('#000');
+    const searchInputTextColor = ref('#000');
+    const menuItemsHoverColor = ref('#000');
+    const isMenuOpen = ref(false);
+
+    return {
+      menuTitle,
+      menuItems,
+      searchPlaceholder,
+      searchTooltip,
+      logoTitleColor,
+      bgColor,
+      iconsColor,
+      secondaryColor,
+      itemsTooltipColor,
+      menuItemsTextColor,
+      searchInputTextColor,
+      menuItemsHoverColor,
+      isMenuOpen
+    };
+  }
+}
+</script>
+
 <style scoped>
+
+.page-container {
+  display: flex;
+  height: 100vh; 
+}
+.sidebar {
+  position: relative;
+  
+}
+
 </style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,17 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: '../src/main/resources/static', // Build Directory
-    devServer: {
-        proxy: {
-          '/':{
-            target:'http://192.168.0.69:8080', // Spring Boot Server
-            changeOrigin:true,
-          },
-          '/oauth2':{
-            target:'http://192.168.0.69:8080', // Spring Boot Servernpm
-            changeOrigin:true,
-            '^/oauth2':'/login/oauth2/code/'
-          }
-        }
-    }
+    // devServer: {
+    //     proxy: {
+    //       '/':{
+    //         target:'http://192.168.0.69:8080', // Spring Boot Server
+    //         changeOrigin:true,
+    //       },
+    //       '/oauth2':{
+    //         target:'http://192.168.0.69:8080', // Spring Boot Servernpm
+    //         changeOrigin:true,
+    //         '^/oauth2':'/login/oauth2/code/'
+    //       }
+    //     }
+    // }
 })
