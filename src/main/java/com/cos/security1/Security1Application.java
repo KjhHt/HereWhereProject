acea178c8c651fbd2d917731a4bfbbca1cc2b1c1
Seung <esybd02@naver.com>
2024-01-23 19:35:54 +0900

    같은곳에서 올리려고할때
    같은곳에서 올리려고할때

--- src/main/java/com/cos/security1/Security1Application.java
+++ src/main/java/com/cos/security1/Security1Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Security1Application {
 
 	public static void main(String[] args) {
+		System.out.println("내가 두번째 계정에서 똑같은곳에 작업하고 커밋하려고할때");
+		System.out.println("어떻게 되는지");
+		System.out.println("테스트를 해보자");
 		SpringApplication.run(Security1Application.class, args);
 	}
 
